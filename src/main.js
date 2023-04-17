386a7918106035874b4cd8878574773e5a53e5a8
Melissa Aquijes <melissa.geral10@gmail.com>
2023-04-17 10:47:10 -0500

    Se agrega link de videos a ghibli.json, titulos y descripción

--- src/main.js
+++ src/main.js
@@ -1,6 +1,44 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+
+const portadaVideo = document.querySelector('.portadaVideo');
+const portadaInfo = document.querySelector('.portadaInfo');
+const titleLogoContainer = document.querySelector('.titleLogoContainer');
+const parrafoContainer = document.querySelector('.parrafoContainer');
+const postersContainer = document.querySelector('.postersContainer');
+
+fetch('./data/ghibli/ghibli.json')
+.then(function(response) {
+  return response.json();
+})
+.then(function(data) {
+  const dataStudioGhibli = data.films;
+  postersGhibli(dataStudioGhibli);
+});
+
+
+const postersGhibli = (ghibliData) => {
+
+  ghibliData.forEach((e) => {
+    const poster= document.createElement('img');
+    poster.setAttribute('src', e.poster)
+    poster.setAttribute('class', 'poster')
+
+    postersContainer.append(poster)
+
+        poster.addEventListener('click', () => {
+            const video = `<video src= ${e.info[0].video} loop autoplay>`
+            const parrafo = `<p class='parrafo'>${e.info[0].texto}</p>`
+            const titleLogo = `<img class= 'imgTitle' src= ${e.info[0].logoTitleUrl} />`
+
+            portadaVideo.innerHTML = video;
+            parrafoContainer.innerHTML = parrafo;
+            titleLogoContainer.innerHTML = titleLogo;
+
+        })
+
+  })
+}
+
+
+>>>>>>> e416070 (Se agrega link de videos a ghibli.json, titulos y descripción)
 
 console.log(example, data);
